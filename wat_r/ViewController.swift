cc619e3a1197188f6755bb747302711bb48cfa1d
Alan Chiem <alanchiem@berkeley.edu>
2021-01-13 00:44:10 -0800

    added timer

--- wat_r/ViewController.swift
+++ wat_r/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    var OurTimer = Timer()
+    var timerDisplayed = 0
+    
+    
+    
+    @IBAction func StartBTN(_ sender: Any) {
+        OurTimer = Timer.scheduledTimer(timeInterval: 1,
+                                        target: self,
+                                        selector: #selector(Action),
+                                        userInfo: nil,
+                                        repeats: true)
+    }
+    
+    
+    @IBAction func PauseBTN(_ sender: Any) {
+        OurTimer.invalidate()
+    }
+    
+    
+    
+    @IBAction func ResetBTN(_ sender: Any) {
+        OurTimer.invalidate()
+        timerDisplayed = 0
+        Label.text = "0"
+    }
+    
+    
+    @objc func Action() {
+        timerDisplayed += 1
+        Label.text = String(timerDisplayed)
+    }
+    
+    
+    @IBOutlet weak var Label: UILabel!
+    
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
