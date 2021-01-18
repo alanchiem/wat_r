ff807c1e000ffb5d58654d0020056d8b2fda2a1f
Alan Chiem <alanchiem@berkeley.edu>
2021-01-17 22:59:48 -0800

    added water drop animation

--- wat_r/ViewController.swift
+++ wat_r/ViewController.swift
@@ -7,16 +7,20 @@
 
 import UIKit
 import Foundation
+import Lottie
 
 class ViewController: UIViewController {
     
     var OurTimer = Timer()
     var timerDisplayed = 0
     
+    // Animation
+    let animationView = AnimationView()
+
     
     
     @IBAction func StartBTN(_ sender: Any) {
-        OurTimer = Timer.scheduledTimer(timeInterval: 1,
+        OurTimer = Timer.scheduledTimer(timeInterval: 2,
                                         target: self,
                                         selector: #selector(Action),
                                         userInfo: nil,
@@ -26,6 +30,7 @@ class ViewController: UIViewController {
     
     @IBAction func PauseBTN(_ sender: Any) {
         OurTimer.invalidate()
+        animationView.pause()
     }
     
     
@@ -34,11 +39,14 @@ class ViewController: UIViewController {
         OurTimer.invalidate()
         timerDisplayed = 0
         Label.text = "0"
+        setupAnimation()
+        animationView.pause()
     }
     
     
     @objc func Action() {
         timerDisplayed += 1
+        animationView.play()
         Label.text = String(timerDisplayed)
     }
     
@@ -49,5 +57,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Animation cont
+        setupAnimation()
+        animationView.pause()
+        
+    }
+    
+    private func setupAnimation() {
+        animationView.animation = Animation.named("falling-drop-of-water")
+        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animationView.center = view.center
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
     }
 }
