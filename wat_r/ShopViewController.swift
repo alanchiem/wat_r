ec245576b8d5d14317b996b92e9f3dbde1b28053
Alan Chiem <alanchiem@berkeley.edu>
2022-03-01 23:07:29 -0800

    added wave animation and drop/time button

--- wat_r/ShopViewController.swift
+++ wat_r/ShopViewController.swift
@@ -10,58 +10,125 @@ import Foundation
 import Lottie
 
 class ShopViewController: UIViewController {
-    // Labels
+    // Shows Drops, would be hidden but is used for debugging
     @IBOutlet var TotalDropsLabel: UILabel!
+
+    // Shows Time, would be hidden but is used for debugging
+    @IBOutlet weak var StatsLabel: UILabel!
     
     // Reset Button: resets total drops accumulated
     @IBOutlet var Reset: UIButton!
-
     
-    @IBOutlet weak var StatsLabel: UILabel!
-    
-    // Score labels
     var TotalDrops = 0
     
-    // Badge Declaration
-    let button = UIButton(frame: CGRect(x: 100, y: 200, width: 75, height: 75))
+    
+    // 36 million drops == 10,000 hours
+
+    
+    // Drops | Time Button (Functionality), would be visible
+    // Allows for the user to switch from displaying drop to displaying time by just tapping
+    var showingDrops = true
+    let button = UIButton(frame: CGRect(x: 100, y: 200, width: 200, height: 40))
+    // when button is tapped
     @objc func buttonAction(sender: UIButton!) {
-      print("Button tapped")
+        if (showingDrops) {
+            showingDrops = false
+            button.setTitle(StatsLabel.text, for: .normal)
+        }
+        else {
+            showingDrops = true
+            button.setTitle(String(TotalDrops), for: .normal)
+        }
     }
     
+    
+    // 1. Create the AnimationView
+    private var animationView: AnimationView?
+    
+    func animationFunction() {
+        // 2. Start AnimationView with animation name (without extension)
+        animationView = .init(name: "newWave")
+        animationView!.frame = CGRect(x: 0, y: 0, width: 420, height: 420)
+        
+        // bottomish area
+        animationView!.center = CGPoint(x: 208, y: 660)
+        
+        // middleish area
+        //animationView!.center = CGPoint(x: 208, y: 260)
+        
+        // 3. Set animation content mode
+        animationView!.contentMode = .scaleAspectFit
+        animationView!.transform = CGAffineTransform(rotationAngle: .pi)
+        
+        // 4. Set animation loop mode
+        animationView!.loopMode = .loop
+        
+        // 5. Adjust animation speed
+        animationView!.animationSpeed = 0.5
+        view.addSubview(animationView!)
+        
+        // 6. Create ColorValueProvider using Lottie's Color class
+        var waveColor = Color(r: (66/255), g: (130/255), b: (174/255), a: 1)
+        
+        if UITraitCollection.current.userInterfaceStyle == .dark {
+                print("Dark mode")
+                waveColor = Color(r: (27/255), g: (83/255), b: (132/255), a: 1)
+            }
+            else {
+                print("Light mode")
+                
+            }
+        
+        let waveColorValueProvider = ColorValueProvider(waveColor)
+
+        // Set color value provider to animation view
+        let keyPath = AnimationKeypath(keypath: "**.Color")
+        animationView!.setValueProvider(waveColorValueProvider, keypath: keyPath)
+        
+        // 7. Play animation
+        animationView!.play()
+        animationView!.backgroundBehavior = .pauseAndRestore
+    }
     
     // Happens everytime view appears
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         dropToTime()
         
-        // Badge Appearance
+        // Drops | Time Button (Atrributes), would be visible
         if (TotalDrops > 0) {
-            button.backgroundColor = .blue
-            button.setTitle("Test Button", for: .normal)
-            button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-            view.addSubview(button)
+            // Color
+            button.backgroundColor = .clear
+            if UITraitCollection.current.userInterfaceStyle == .dark {
+                button.setTitleColor(.white, for: .normal)
             }
-        if (TotalDrops == 0) {
-            button.removeFromSuperview()
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            else {
+                button.setTitleColor(.black, for: .normal)
+            }
+            button.setTitleColor(.lightGray, for: .highlighted)
+            
+            }
+            
+            // Text
+            button.setTitle(String(TotalDrops), for: .normal)
+            button.titleLabel!.font = UIFont(name: "AppleSDGothicNeo-Thin" , size: 25)
+            
+            // Action
+            button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+            // Positioning
+            button.center = self.view.center
+            
+            view.addSubview(button)
+            
+ 
         
     }
     
     // When code launches Only happens once
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
         // set up exchange data for stopwatch
         NotificationCenter.default.addObserver(self, selector: #selector(notificationForStopwatch(_:)), name: Notification.Name("text"), object: nil)
@@ -71,7 +138,9 @@ class ShopViewController: UIViewController {
             TotalDropsLabel.text = String(TotalDrops)
         }
         dropToTime()
-
+        
+        animationFunction()
+        
         }
 
     
@@ -113,11 +182,12 @@ class ShopViewController: UIViewController {
     
     // Reset Button
     @IBAction func ResetAction(sender: AnyObject) {
-        TotalDrops = 0
+        TotalDrops = 123456789
         let DropDefault = UserDefaults.standard
         DropDefault.setValue(TotalDrops, forKey: "TotalDrops")
         DropDefault.synchronize()
         TotalDropsLabel.text = String(TotalDrops)
+        button.setTitle(String(TotalDrops), for: .normal)
     }
     
 }
